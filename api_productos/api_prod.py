231252cdbce37e88737a13ba11c404a4eaf7bea5
Eduardo Calabrano <ecalabrano@linets.cl>
2024-02-03 18:44:13 -0300

    avances en post de producto

--- api_productos/api_prod.py
+++ api_productos/api_prod.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
+from typing import Optional
+from uuid import uuid4 as uuid
+from pydantic import BaseModel
 
+
+class Producto(BaseModel):
+    #id: Optional[str] = None
+    id: str | None = None
+    nombre: str
+    precio_compra: float
+    precio_venta: float
+    proveedor: str
 
 app = FastAPI()
+productos = []
 
 @app.get('/')
 def index():
     return {'mensaje': 'Bienvenidos a la API de Productos'}
+
+@app.get('/producto')
+def obtener_productos():
+    return productos
+
+@app.post('/producto')
+def crear_producto(producto: Producto):
+    producto.id = str(uuid())
+    productos.append(producto)
+    return {'mensaje': 'Producto creado satisfactoriamente'}
