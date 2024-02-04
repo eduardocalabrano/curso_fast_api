350d4d53313a1c55378cb318d0d08b704dd73b06
Eduardo Calabrano <ecalabrano@linets.cl>
2024-02-04 18:54:56 -0300

    se agrega delete y put

--- api_productos/api_prod.py
+++ api_productos/api_prod.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from typing import Optional
 from uuid import uuid4 as uuid
 from pydantic import BaseModel
@@ -32,7 +32,31 @@ def crear_producto(producto: Producto):
 
 @app.get('/producto/{producto_id}')
 def obtener_prod_por_id(producto_id: str):
-    for p in productos:
-        if p.id == producto_id:
-            return p
-    return {'mensaje': f'El producto con el ID {producto_id} no existe'}
+    resultado = list(filter(lambda p: p.id == producto_id, productos))
+    if len(resultado):
+        return resultado[0]
+    raise HTTPException(status_code=404, detail=f'El producto no se encuentra')
+
+@app.delete('/producto/{producto_id}')
+def eliminar_producto_por_id(producto_id: str):
+    resultado = list(filter(lambda p: p.id == producto_id, productos))
+    if len(resultado):
+        producto = resultado[0]
+        productos.remove(producto)
+        return {'mensaje': f'El producto con el id {producto_id} fue eliminado'}
+    raise HTTPException(status_code=404, detail=f'El producto no se encuentra')
+
+
+@app.put('/producto/{producto_id}')
+def actualizar_producto(producto_id: str, producto: Producto):
+    resultado = list(filter(lambda p: p.id == producto_id, productos))
+
+    if len(resultado):
+        producto_encontrado = resultado[0]
+        producto_encontrado.nombre = producto.nombre
+        producto_encontrado.precio_compra = producto.precio_compra
+        producto_encontrado.precio_venta = producto.precio_venta
+        producto_encontrado.proveedor = producto.proveedor
+
+        return producto_encontrado
+    raise HTTPException(status_code=404, detail=f'El producto no se encuentra')
